0278627c977d3a7e7c36cc34366978aa8307a4b0
matthwk <110775063+matthwk@users.noreply.github.com>
2024-02-17 10:39:59 +0700

    Delete vania-frontend directory

--- vania-frontend/src/constants/Constanta.js
+++
@@ -1,8 +0,0 @@
-export function handleNumberFormatCurrency (payload) {
-    return new Intl.NumberFormat('id').format(payload);
-}
-
-export function handleAvailability (payload) {
-    if (payload) return "Available";
-    return "Not Available"
-}
--- vania-frontend/src/reducers/CombineReducers.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import {menuCategoryReducer} from "./MenuCategoryReducer";
-import {menuReducer} from "./MenuReducer";
-
-
-export const reducers = combineReducers({
-    menuCategoryReducer,
-    menuReducer,
-});
